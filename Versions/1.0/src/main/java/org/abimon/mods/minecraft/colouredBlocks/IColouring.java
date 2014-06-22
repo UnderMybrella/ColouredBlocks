ba73a48b63b338bcb57b4e03f1cf9661f15cae4d
Isaac (UnderMybrella) <Heathcliffe@Isaacs-iMac.local>
2014-06-22 13:56:38 -0700

    Version 1.0
    Added all the things

---
+++ Versions/1.0/src/main/java/org/abimon/mods/minecraft/colouredBlocks/IColouring.java
@@ -0,0 +1,10 @@
+package org.abimon.mods.minecraft.colouredBlocks;
+
+import java.awt.Color;
+
+import net.minecraft.item.ItemStack;
+
+public interface IColouring 
+{
+	public Color getColour(ItemStack item);
+}
